16e938c5660db189c4960b654161eff06c7f2c80
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-12-19 12:37:25 +0900

    星座のデータベース作成

---
+++ Assets/_MyAssets/Scripts/DataBase/Constellation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DataBase
+{
+
+    public class Constellation
+    {
+        public string id;
+        public string constellation_name;
+        public string start;
+        public string end;
+    }
+}
