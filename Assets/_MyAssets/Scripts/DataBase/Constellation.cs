f55772c0c0941cb6e01736e5357107909227eabf
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-12-19 12:46:32 +0900

    日付テキストをdatetimeに変換

--- Assets/_MyAssets/Scripts/DataBase/Constellation.cs
+++ Assets/_MyAssets/Scripts/DataBase/Constellation.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 
 namespace DataBase
 {
-
     public class Constellation
     {
         public string id;
         public string name;
         public string start;
         public string end;
+
+        public DateTime StartDT => Convert.ToDateTime(start);
+        public DateTime EndDT => Convert.ToDateTime(end);
     }
 }
