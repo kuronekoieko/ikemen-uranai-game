7caa94bee26070fb42d8afea0cf4d87a41a8335e
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-12-30 06:59:07 +0900

    シーン移行

---
+++ Assets/_MyAssets/Scripts/Scenes/InitializeScene/Initialize.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InitializeScene
+{
+    public class Initialize : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+
+    }
+
+}
