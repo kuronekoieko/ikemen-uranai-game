bab93c2df14f03f21207af043dfb5228a8982cd5
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2023-12-31 10:58:02 +0900

    firebaseの設定ファイルを、開発環境と本番環境で切り替え

---
+++ Assets/_MyAssets/Scripts/Editor/FirebaseConfigManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using System.IO;
+using UnityEditor;
+
+
+public class FirebaseConfigManager
+{
+    // Androidの設定ファイル
+    static readonly string ANDROID_SETTINGS = "google-services.json";
+    // iOSの設定ファイル
+    static readonly string IOS_SETTINGS = "GoogleService-Info.plist";
+    // Firebaseの設定ファイルのパス
+    static readonly string firebasePath = Application.dataPath + "/_MyAssets/FirebaseSettingFiles";
+
+
+    [MenuItem("MyTool/Create Firebase Settings")]
+    public static void CreateFiles()
+    {
+        string baseAndroidSettings;
+        string baseIosSettings;
+
+        if (EditorUserBuildSettings.development)
+        {
+            baseAndroidSettings = "google-services-dev.json";
+            baseIosSettings = "GoogleService-Info-dev.plist";
+        }
+        else
+        {
+            baseAndroidSettings = "google-services-prod.json";
+            baseIosSettings = "GoogleService-Info-prod.plist";
+        }
+
+        // Firebaseの設定ファイルを上書きコピー
+        // File.Copy(firebasePath + baseAndroidSettings, firebasePath + ANDROID_SETTINGS, overwrite: true);
+        // File.Copy(firebasePath + baseIosSettings, firebasePath + IOS_SETTINGS, overwrite: true);
+        FileUtil.CopyFileOrDirectory(firebasePath + baseAndroidSettings, firebasePath + ANDROID_SETTINGS);
+        FileUtil.CopyFileOrDirectory(firebasePath + baseIosSettings, firebasePath + IOS_SETTINGS);
+
+    }
+
+}
