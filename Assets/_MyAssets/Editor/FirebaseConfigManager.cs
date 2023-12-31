cb4026a6cd147d08145e3e53ff14e49f6f2065b6
家子智成 <>
2023-12-31 11:34:35 +0900

    unityeditornamespaceエラー修正

--- Assets/_MyAssets/Editor/FirebaseConfigManager.cs
+++ Assets/_MyAssets/Editor/FirebaseConfigManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
 using System.IO;
 using UnityEditor;
 
