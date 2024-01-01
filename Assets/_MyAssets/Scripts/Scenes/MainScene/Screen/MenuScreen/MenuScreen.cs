a841bca29fa786bef417baa7629e6f374b2897b1
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2024-01-01 20:21:24 +0900

    デバッグ用にログイン、ログアウト

--- Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/MenuScreen/MenuScreen.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/MenuScreen/MenuScreen.cs
@@ -1,21 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuScreen : BaseScreen
 {
     [SerializeField] Button closeButton;
+    [SerializeField] Button signInButton;
+    [SerializeField] Button signOutButton;
+    [SerializeField] TextMeshProUGUI uidText;
 
     public override void OnStart()
     {
         base.OnStart();
         closeButton.onClick.AddListener(Close);
+        signInButton.onClick.AddListener(async () =>
+        {
+            await FirebaseAuthenticationManager.Instance.Initialize();
+            uidText.text = "uid: " + FirebaseAuthenticationManager.Instance.User.UserId;
+        });
+        signOutButton.onClick.AddListener(async () =>
+        {
+            await FirebaseAuthenticationManager.Instance.DeleteAsync();
+            FirebaseAuthenticationManager.Instance.SignOut();
+            uidText.text = "uid: ";
+        });
+
     }
 
     public override void Open()
     {
         base.Open();
+
+        if (FirebaseAuthenticationManager.Instance.User == null)
+        {
+            uidText.text = "uid: ";
+        }
+        else
+        {
+            uidText.text = "uid: " + FirebaseAuthenticationManager.Instance.User.UserId;
+        }
     }
 
     public override void Close()
