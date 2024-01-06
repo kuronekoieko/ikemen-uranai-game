344d1c21ca1a696be733d93e1b17fa9d0dc16edc
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2024-01-06 22:08:32 +0900

    ホームでキャラをタップしたときに、会話を開始

---
+++ Assets/Naninovel/Runtime/MyCommand/EndScriptCommand.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Naninovel;
+using UnityEngine;
+
+[CommandAlias("endScript")] // このエイリアス名でスクリプトからコマンドを呼び出す(@endScript)
+public class EndScriptCommand : Command
+{
+    public static event Action<string> OnScriptEnded;
+    //static bool isRunningNaniNovelScript;
+
+    public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+    {
+        // IScriptPlayerサービスを取得
+        var scriptPlayer = Engine.GetService<IScriptPlayer>();
+
+        // 現在再生中のスクリプトの名前を取得
+        string currentScriptName = scriptPlayer.PlayedScript.Name;
+
+        Debug.Log($"NaniNovelスクリプト '{currentScriptName}' が終了しました。");
+
+        //NaniNovel関連オブジェクトを非アクティブ化
+        // ActivateNaniNovelObjects(false);
+
+        // イベントをトリガーして、スクリプトが終了したことを通知
+        OnScriptEnded?.Invoke(currentScriptName);
+
+        // NaniNovelスクリプトが実行中でないことにする（後述）
+        //isRunningNaniNovelScript = false;
+
+        return UniTask.CompletedTask;
+    }
+}
