a33e81bf1c3347ecc67ff648fa80df45fdd56fd3
tomonari_ieko <tomonari.ieko.personal@gmail.com>
2024-01-07 18:41:51 +0900

    リファクタ

--- Assets/_MyAssets/Scripts/Utils/AssetBundleLoader.cs
+++ Assets/_MyAssets/Scripts/Utils/AssetBundleLoader.cs
@@ -6,31 +6,9 @@ using Cysharp.Threading.Tasks;
 using System;
 using UnityEngine.AddressableAssets;
 using DataBase;
-using PlasticPipe.PlasticProtocol.Messages;
 
 public class AssetBundleLoader
 {
-
-    public static async UniTask<T> DownloadAssetBundleAsync<T>(string url, string assetBundleName) where T : UnityEngine.Object
-    {
-        if (string.IsNullOrEmpty(url)) return null;
-
-        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
-        await request.SendWebRequest();
-
-        if (request.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(request.error);
-            return null;
-        }
-        else
-        {
-            AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(request);
-            var obj = await assetBundle.LoadAssetAsync(assetBundleName);
-            return obj as T;
-        }
-    }
-
 
     public static async UniTask<T> LoadAddressablesAsync<T>(string address) where T : UnityEngine.Object
     {
@@ -41,19 +19,12 @@ public class AssetBundleLoader
         {
             await Addressables.LoadAssetAsync<T>(address).Task;
         }
-        catch (Exception e)
+        catch (Exception)
         {
             Debug.LogError("ロード失敗 " + address);
         }
         // Debug.Log("ロード終了");
         return a;
-    }
-
-    public static async UniTask<T> DownloadAddressablesAsync<T>(string address) where T : UnityEngine.Object
-    {
-        Addressables.WebRequestOverride = EditWebRequestURL;
-        var a = await Addressables.DownloadDependenciesAsync(address).Task;
-        return a as T;
     }
 
     // https://gist.github.com/anmq0502/a229a048f27c91e775aeabf40517a1bd
