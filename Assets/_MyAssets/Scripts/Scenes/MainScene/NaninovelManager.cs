efd408ed9b94c8c1befe035aa6d6e0ac5074706d
ieko <tomonari.ieko@gmail.com>
2024-02-03 22:10:10 +0900

    キー修正

--- Assets/_MyAssets/Scripts/Scenes/MainScene/NaninovelManager.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/NaninovelManager.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Naninovel;
 
-public class NaninovelManager : MonoBehaviour
+public class NaninovelManager
 {
     public static async Cysharp.Threading.Tasks.UniTask InitializeAsync(int characterId)
     {
         Debug.Log("Naninovel読み込み開始");
-
         // https://naninovel.com/ja/guide/integration-options#%E6%89%8B%E5%8B%95%E5%88%9D%E6%9C%9F%E5%8C%96
         await RuntimeInitializer.InitializeAsync();
         await PlayHomeAsync(characterId);
@@ -24,6 +23,7 @@ public class NaninovelManager : MonoBehaviour
 
     public static async Cysharp.Threading.Tasks.UniTask PlayAsync(string scriptName)
     {
+        Debug.Log(scriptName + ".nani");
         var player = Engine.GetService<IScriptPlayer>();
         // ツールバー Naninovel -> Resources -> Scripts でスクリプト割当
         await player.PreloadAndPlayAsync(scriptName + ".nani");
