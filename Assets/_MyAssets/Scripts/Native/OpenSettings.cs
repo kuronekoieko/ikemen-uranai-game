bebc1e5b2801468151242dd24bbb93580fbd974a
家子智成 <>
2024-02-04 19:17:55 +0900

    ビルドエラー修正

--- Assets/_MyAssets/Scripts/Native/OpenSettings.cs
+++ Assets/_MyAssets/Scripts/Native/OpenSettings.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if !UNITY_EDITOR && UNITY_IOS
+using System.Runtime.InteropServices;
+#endif
 
 public static class OpenSettings 
 {
