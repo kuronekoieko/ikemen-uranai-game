6b24a0ed7595a06f89b93f38b6af7f6d7ed9731d
ieko <tomonari.ieko@gmail.com>
2024-02-04 15:50:29 +0900

    祝日が含まれるかの判定

--- Assets/_MyAssets/Scripts/DataBase/CSVManager.cs
+++ Assets/_MyAssets/Scripts/DataBase/CSVManager.cs
@@ -21,7 +21,7 @@ public static class CSVManager
     public static LuckyItem[] LuckyItems { get; private set; }
     public static LuckyColor[] LuckyColors { get; private set; }
     public static ChargingProduct[] ChargingProducts { get; private set; }
-    static HomeText[] HomeTexts;
+    public static HomeText[] HomeTexts { get; private set; }
 
     public static async UniTask InitializeAsync()
     {
@@ -178,57 +178,6 @@ public static class CSVManager
         return csvDatas;
     }
 
-    public static HomeText GetHomeText(DateTime dateTime)
-    {
 
-        bool isHoliday = false;
-
-        var group = HomeTexts
-            .Where(homeText =>
-            {
-                if (homeText.date.priority == 0) return true;
-                if (DateTime.TryParse(homeText.date.date, out DateTime dateDT))
-                {
-                    return dateDT == dateTime.Date;
-                }
-                else
-                {
-                    return false;
-                }
-            })
-            .Where(homeText =>
-            {
-                if (homeText.day.priority == 0) return true;
-                return homeText.day.IsIncludeDay(dateTime.DayOfWeek);
-            })
-            .Where(homeText =>
-            {
-                if (homeText.time.priority == 0) return true;
-                TimeSpan startTimeOfDay = homeText.time.StartTimeOfDay();
-                TimeSpan endTimeOfDay = homeText.time.EndTimeOfDay();
-
-                // Debug.Log(startTimeOfDay + " - " + endTimeOfDay);
-
-                return startTimeOfDay <= dateTime.TimeOfDay && dateTime.TimeOfDay <= endTimeOfDay;
-            })
-            .GroupBy(homeText => homeText.Priority)
-            .OrderByDescending(group => group.Key)
-            .FirstOrDefault();
-
-        if (group == null)
-        {
-            Debug.LogError("ホーム会話がみつかりません");
-            return null;
-        }
-
-        foreach (var homeText in group.ToArray())
-        {
-            // DebugUtils.LogJson(homeText);
-            // Debug.Log(homeText.date.date_name + " " + homeText.day.day_name + " " + homeText.time.time_name + " " + homeText.Priority);
-        }
-        //var randomHomeText = group.ToArray().GetRandom();
-
-        return group.ToArray().GetRandom();
-    }
 }
 
