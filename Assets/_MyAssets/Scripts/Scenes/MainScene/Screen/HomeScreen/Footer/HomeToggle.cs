6131724cea4954da98a5423d252f80c14cceed97
ieko <tomonari.ieko@gmail.com>
2024-03-17 21:43:53 +0900

    占いボタンの処理リファクタリング、既読状況をセーブデータの初期化を待ってから反映するように

--- Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Footer/HomeToggle.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Footer/HomeToggle.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Cysharp.Threading.Tasks;
 
 public class HomeToggle : BaseFooterToggleController
 {
@@ -10,17 +11,21 @@ public class HomeToggle : BaseFooterToggleController
     {
         base.OnStart();
         base.SetSelectedAction(Home);
-        this.ObserveEveryValueChanged(isShowBadge => IsShowBadge())
-            .Subscribe(isShowBadge => base.footerToggle.SetActiveBadge(isShowBadge))
+        this.ObserveEveryValueChanged(async isShowBadgeAsync => await IsShowBadgeAsync())
+            .Subscribe(async isShowBadgeAsync =>
+            {
+                bool isShowBadge = await isShowBadgeAsync;
+                base.footerToggle.SetActiveBadge(isShowBadge);
+            })
             .AddTo(gameObject);
     }
 
 
-    bool IsShowBadge()
+    async UniTask<bool> IsShowBadgeAsync()
     {
         if (PageManager.Instance == null) return false;
-        bool isNotificationTodayHoroscope = PageManager.Instance.Get<HomePage>().IsNotificationTodayHoroscope();
-        bool isNotificationNextDayHoroscope = PageManager.Instance.Get<HomePage>().IsNotificationNextDayHoroscope();
+        bool isNotificationTodayHoroscope = await PageManager.Instance.Get<HomePage>().IsNotificationTodayHoroscope();
+        bool isNotificationNextDayHoroscope = await PageManager.Instance.Get<HomePage>().IsNotificationNextDayHoroscope();
         return isNotificationTodayHoroscope || isNotificationNextDayHoroscope;
     }
 
---
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Page/HomePage/BaseHoroscopeButton.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+using SaveDataObjects;
+
+
+public abstract class BaseHoroscopeButton : MonoBehaviour
+{
+    [SerializeField] protected Button button;
+    protected string Key => DateTime.Today.ToDateKey();
+
+    public virtual void OnStart()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    public virtual void OnOpen()
+    {
+
+    }
+
+    protected async UniTask<HoroscopeHistory> GetHoroscopeHistory()
+    {
+        await UniTask.WaitUntil(() => SaveDataManager.SaveData != null);
+        await UniTask.WaitUntil(() => SaveDataManager.SaveData.horoscopeHistories != null);
+
+        SaveDataManager.SaveData.horoscopeHistories.TryGetValue(Key, out HoroscopeHistory horoscopeHistory);
+        return horoscopeHistory;
+    }
+
+    protected void Anim()
+    {
+        transform
+            .DOPunchScale(Vector3.one * 0.1f, 2f, 3, 0.1f)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1)
+            .OnKill(() =>
+            {
+                transform.localScale = Vector3.one;
+            });
+    }
+
+    protected abstract void OnClick();
+
+}
