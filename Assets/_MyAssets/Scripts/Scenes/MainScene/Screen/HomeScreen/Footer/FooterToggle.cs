0c9a6616b7e97ae1405119f69ed2256e104cbfae
ieko <tomonari.ieko@gmail.com>
2024-03-17 18:30:35 +0900

    フッターの処理を、ボタンからtoggleに移行

--- Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Footer/FooterToggle.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Footer/FooterToggle.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class FooterToggle : MonoBehaviour
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+
+public class FooterToggle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
     [SerializeField] Toggle toggle;
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Image badgeImage;
+    readonly float duration = 0.2f;
+
+
     public void OnStart(FooterToggleData footerToggleData)
     {
         text.text = footerToggleData.name;
         toggle.image.sprite = footerToggleData.offSprite;
-        toggle.graphic.GetComponent<Image>().sprite = footerToggleData.onSprite;
+        if (toggle.graphic.TryGetComponent(out Image graphicImage))
+        {
+            graphicImage.sprite = footerToggleData.onSprite;
+        }
+        toggle.onValueChanged.AddListener((isOn) =>
+        {
+            if (isOn) footerToggleData.onSelected.Invoke();
+        });
+        toggle.interactable = footerToggleData.interactable;
+        toggle.gameObject.SetActive(footerToggleData.active);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (toggle.interactable == false) return;
+        transform.DOScale(Vector3.one * 1.1f, duration);
     }
 
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one, duration);
+    }
 
 }
