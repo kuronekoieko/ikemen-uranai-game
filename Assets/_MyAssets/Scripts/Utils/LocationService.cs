f14e6bffd9bd2f5c4bc4041eefae91b7e8fa1c7d
家子智成 <>
2024-03-21 18:02:22 +0900

    位置情報ダイアログを閉じたあとに、必ず設定画面を開くように

--- Assets/_MyAssets/Scripts/Utils/LocationService.cs
+++ Assets/_MyAssets/Scripts/Utils/LocationService.cs
@@ -7,9 +7,13 @@ using UnityEngine;
 public class LocationService
 {
     // https://docs.unity3d.com/ja/current/ScriptReference/LocationService.Start.html
+
+    // Unity iOS/Androidで位置情報を取得する
+    // https://qiita.com/hirano/items/dde92f4ed76fb377746e
     public static async void Start()
     {
-        // Check if the user has location service enabled.
+        // unityエディタ上では通る
+        // iosだと、「許可しない」にしても通らない
         if (!Input.location.isEnabledByUser)
         {
             Debug.Log("LocationService: " + "not enabled by user");
@@ -17,7 +21,18 @@ public class LocationService
             return;
         }
 
+
+        // 仕様:
+        // ボタンを押したらダイアログを出す
+        // ダイアログが出ない場合、設定画面に移動する
+        // ダイアログが出たかどうか判定する方法が無い
+
         // Starts the location service.
+        // 未設定or「許可しない」or「一度だけ許可」の場合にダイアログ出る
+        // ダイアログ「一度だけ許可」Running、「アプリの使用中は許可」Running、「許可しない」Failed→もう一度押す→ダイアログ出ない
+        // 設定画面「しない」→もう一度押す→ダイアログ出ない:Failed
+        // 設定画面「次回または共有時に確認」→もう一度押す→ダイアログ出る:Initializing
+        // 設定画面「このアプリを使用中」→もう一度押す→ダイアログ出ない:Running
         Input.location.Start();
 
         // Waits until the location service initializes
@@ -32,21 +47,26 @@ public class LocationService
             return;
         }
 
+        OpenSettings.Open();
+
+
         Debug.Log("LocationService: " + Input.location.status);
 
         switch (Input.location.status)
         {
             case LocationServiceStatus.Stopped:
+                // Input.location.Stop();すると通る
                 break;
             case LocationServiceStatus.Running:
+                // 許可するとここ通る
                 break;
             case LocationServiceStatus.Failed:
-                OpenSettings.Open();
+                // 「許可しない」押すと通る
                 break;
             default:
                 break;
         }
         Input.location.Stop();
-        Debug.Log("LocationService: " + Input.location.status);
+        // Debug.Log("LocationService: " + Input.location.status);
     }
 }
