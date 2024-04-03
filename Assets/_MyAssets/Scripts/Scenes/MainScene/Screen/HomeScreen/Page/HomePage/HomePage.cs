c55cddb26e366540bca5250129da5f07c0735328
ieko <tomonari.ieko@gmail.com>
2024-04-03 16:24:55 +0900

    サイドバナーのアニメーション修正

--- Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Page/HomePage/HomePage.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/Screen/HomeScreen/Page/HomePage/HomePage.cs
@@ -15,6 +15,8 @@ public class HomePage : BasePage
     [SerializeField] Button selectCharacterScreenButton;
     [SerializeField] Button debugButton;
     [SerializeField] SideBanner sideBanner;
+    [SerializeField] Button popupButton;
+
 
 
     public override void OnStart()
@@ -35,6 +37,16 @@ public class HomePage : BasePage
             return UniTask.DelayFrame(0);
         });
         debugButton.gameObject.SetActive(Debug.isDebugBuild);
+        popupButton.AddListener(async () =>
+        {
+            await PopupManager.Instance.GetCommonPopup().ShowAsync(
+"",
+"メンテナンス中です\nしばらく時間をおいてお試しください。",
+"OK",
+"キャンセル"
+);
+
+        });
     }
 
     async UniTask OnClickCharacter()
