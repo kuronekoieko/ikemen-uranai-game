1ba9150500fe1a11d879ca8b70010f945af993e7
ieko <tomonari.ieko@gmail.com>
2024-04-04 14:17:28 +0900

    トグルをクリックしたときに、サイドバナーのアニメーションが動くバグ修正

--- Assets/_MyAssets/Scripts/Extensions/AnimatorExtensions.cs
+++ Assets/_MyAssets/Scripts/Extensions/AnimatorExtensions.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 
 public static class AnimatorExtensions
 {
-    public async static UniTask SetTriggerAsync(this Animator self, string stateName, string triggerName)
+    public async static UniTask SetTriggerAsync(this Animator self, string triggerName)
     {
         self.SetTrigger(triggerName);
-        //animator.Play("CloseWindow");
-
-        await UniTask.WaitUntil(() =>
-            self.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
-            self.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f
-        ); // animationが終わるまで遅延
-
+        await UniTask.WaitUntil(() => self.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
     }
 
     public async static UniTask PlayAsync(this Animator self, string stateName, int layer = 0)
