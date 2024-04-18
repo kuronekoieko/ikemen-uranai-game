13dfa657d3f929ae780410e343bd1af5f5979c75
ieko <tomonari.ieko@gmail.com>
2024-04-18 17:16:39 +0900

    サウンド初期化

--- Assets/_MyAssets/Scripts/Scenes/MainScene/AudioManager.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/AudioManager.cs
@@ -1,27 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
-    AudioSource audioSource
-    {
-        get
-        {
-            if (_audioSource == null) _audioSource = GetComponent<AudioSource>();
-            return _audioSource;
-        }
-    }
-    AudioSource _audioSource;
+    [SerializeField] AudioSource audioSource;
 
+
+    AudioDataSO audioDataSO;
+
+    public async UniTask Initialize()
+    {
+        var obj = await Resources.LoadAsync<AudioDataSO>("ScriptableObjects/AudioDataSO.asset");
+        audioDataSO = obj as AudioDataSO;
+    }
 
     public void PlayOneShot(AudioClip audioClip)
     {
         // if (SaveData.Instance.isOffSE) { return; }
-        //if (SoundResourceSO.Instance.resources.Length - 1 < resourceIndex) { return; }
-        //AudioClip clip = SoundResourceSO.Instance.resources[resourceIndex].audioClip;
         if (audioClip == null) { return; }
         audioSource.PlayOneShot(audioClip);
+    }
+
+    public void PlayOneShot(AudioID audioID)
+    {
+        if (audioDataSO == null) { return; }
+        AudioClip audioClip = audioDataSO.audioDatas.FirstOrDefault(audioData => audioData.audioID == audioID).audioClip;
+        PlayOneShot(audioClip);
     }
 }
---
+++ Assets/_MyAssets/Scripts/ScriptableObjects/AudioDataSO.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyGame/Create " + nameof(AudioDataSO), fileName = nameof(AudioDataSO))]
+public class AudioDataSO : ScriptableObject
+{
+    public AudioData[] audioDatas;
+}
+
+[Serializable]
+public class AudioData
+{
+    public AudioID audioID;
+    public AudioClip audioClip;
+}
+
+public enum AudioID
+{
+    BtnClick,
+}
