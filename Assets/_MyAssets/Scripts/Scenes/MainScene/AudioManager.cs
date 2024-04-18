da04e19a8018627e7cf7da726504c1d3d141fe58
ieko <tomonari.ieko@gmail.com>
2024-04-18 18:51:31 +0900

    BGM再生

--- Assets/_MyAssets/Scripts/Scenes/MainScene/AudioManager.cs
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/AudioManager.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class AudioManager : SingletonMonoBehaviour<AudioManager>
 {
-    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource soundEffectAS;
+    [SerializeField] AudioSource backgroundMusicAS;
+
     AudioDataSO audioDataSO;
-    readonly string audioDataSOName = "AudioDataSO";
-    readonly string audioDataSOName_NotCompressed = "AudioDataSO_NotCompressed";
+    readonly string audioDataSO_Compressed = "AudioDataSO_Compressed";
+    readonly string audioDataSO_NotCompressed = "AudioDataSO_NotCompressed";
 
     public async UniTask Initialize()
     {
-        string path = "ScriptableObjects/" + audioDataSOName_NotCompressed;
+        string path = "ScriptableObjects/" + audioDataSO_Compressed;
         // パスに拡張子つけない
         var obj = await Resources.LoadAsync<AudioDataSO>(path);
 
@@ -25,23 +26,39 @@ public class AudioManager : SingletonMonoBehaviour<AudioManager>
         }
     }
 
+    public void Play(AudioID audioID)
+    {
+        AudioClip audioClip = GetAudioClip(audioID);
+        if (audioClip == null) { return; }
+        backgroundMusicAS.clip = audioClip;
+        backgroundMusicAS.Play();
+    }
+
     public void PlayOneShot(AudioClip audioClip)
     {
         // if (SaveData.Instance.isOffSE) { return; }
         if (audioClip == null) { return; }
-        audioSource.PlayOneShot(audioClip);
+        soundEffectAS.PlayOneShot(audioClip);
     }
 
     public void PlayOneShot(AudioID audioID)
     {
         if (audioDataSO == null) { return; }
+        if (audioID == AudioID.None) { return; }
+
+        AudioClip audioClip = GetAudioClip(audioID);
+
+        PlayOneShot(audioClip);
+    }
+
+    AudioClip GetAudioClip(AudioID audioID)
+    {
         AudioData audioData = audioDataSO.audioDatas.FirstOrDefault(audioData => audioData.audioID == audioID);
         if (audioData == null)
         {
             Debug.LogError("AudioManager audioID: " + audioID + " がありません");
-            return;
+            return null;
         }
-
-        PlayOneShot(audioData.audioClip);
+        return audioData.audioClip;
     }
 }
