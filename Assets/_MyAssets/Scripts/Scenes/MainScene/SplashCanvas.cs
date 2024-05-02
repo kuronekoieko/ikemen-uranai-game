58c196b48c536f4a331aff86bf8093609d88c146
家子智成 <>
2024-05-02 12:18:48 +0900

    スプラッシュにアニメーション

---
+++ Assets/_MyAssets/Scripts/Scenes/MainScene/SplashCanvas.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashCanvas : SingletonMonoBehaviour<SplashCanvas>
+{
+    [SerializeField] CanvasGroup splashCG;
+
+    private void Awake()
+    {
+        splashCG.alpha = 0;
+    }
+
+    public void Open()
+    {
+        splashCG.DOFade(1, 1f);
+    }
+
+    public async UniTask Close()
+    {
+        await splashCG.DOFade(0, 1f).AsyncWaitForCompletion();
+        gameObject.SetActive(false);
+    }
+}
